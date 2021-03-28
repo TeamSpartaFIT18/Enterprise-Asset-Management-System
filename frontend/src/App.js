05fcf560ebf709007f274a1e97b1478d3fe1d031
Pasindu Premachandra <pasindug98@gmail.com>
2021-03-29 04:02:06 +0530

    Shopping Cart added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import OneProductScreen from './screens/OneProductScreen/OneProductScreen';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import OneProductScreen from './screens/OneProductScreen/OneProductScreen'
+import CartScreen from './screens/CartScreen'
 
 const App = () => {
-  return (
-    <Router>
-      <NavBar />
-      <main className='py-3'>
-        <Container>
-          <Route path='/' component={HomeScreen} exact />
-          <Route path='/products' component={ProductScreen} exact />
-          <Route path='/product/:id' component={OneProductScreen} />
-        </Container>
-      </main>
-      <Footer />
-    </Router>
-  );
-};
+	return (
+		<Router>
+			<NavBar />
+			<main className='py-3'>
+				<Container>
+					<Route path='/' component={HomeScreen} exact />
+					<Route path='/products' component={ProductScreen} exact />
+					<Route path='/product/:id' component={OneProductScreen} />
+					<Route path='/cart/:id?' component={CartScreen} />
+				</Container>
+			</main>
+			<Footer />
+		</Router>
+	)
+}
 
-export default App;
+export default App
