8fb8d288b1cc1ec8ae416f90d4a9ca8bc371165c
Sachini Senanayake <48913793+sachini1997@users.noreply.github.com>
2021-04-03 14:06:21 +0530

    order

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,32 +1,79 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product/Product'
 import axios from 'axios'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import Paginate from '../components/Paginate'
 
-const ProductScreen = () => {
-	const [products, setProducts] = useState([])
+import { listProducts } from '../actions/productActions'
 
-	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get('/api/products')
+const ProductScreen = ({match}) => {
+	
+		const [products, setProducts] = useState([])
 
-			setProducts(data)
-		}
-		fetchProducts()
-	}, [])
+		useEffect(() => {
+			const fetchProducts = async () => {
+				const { data } = await axios.get('/api/products')
 
+				setProducts(data)
+			}
+			fetchProducts()
+		}, [])
+	
+		
+	/*const keyword = match.params.keyword
+
+		const pageNumber = match.params.pageNumber || 1
+
+		const dispatch = useDispatch()
+
+		const productList = useSelector((state) => state.productList)
+		const { loading, error, productss, page, pages } = productList
+
+		useEffect(() => {
+		dispatch(listProducts(keyword, pageNumber))
+		}, [dispatch, keyword, pageNumber])
+  
+	*/
 	return (
 		<>
+			
+      	
+			<Link to='/' className='btn btn-light'>
+			Go Back
+			</Link>
+      	
 			<h1>LATEST PRODUCTS</h1>
-			<Row>
-				{products.map(product => (
-					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+
+			
+			  <Loader />
+		
+			
+				<>
+					<Row>
+						{products.map(product => (
+							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+								<Product product={product} />
+							</Col>
+						))}
+					</Row>
+					
+				</>	
+		    
+	
 		</>
 	)
 }
 
 export default ProductScreen
+
+/*
+					<Paginate
+					pages={pages}
+					page={page}
+					keyword={keyword ? keyword : ''}
+					/>
+					*/
