14601ad9c9f8470f871f864562baee645b710015
Thisal <thisal.18@itfac.mrt.ac.lk>
2021-05-07 12:05:52 +0530

    edit user screen

--- frontend/src/screens/UserEditScreen/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen/UserEditScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Card, Row, Col, Image } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../../components/Message'
 import Loader from '../../components/Loader'
@@ -8,6 +8,7 @@ import FormContainer from '../../components/FormContainer'
 import { getUserDetails, updateUser } from '../../actions/userActions'
 import { USER_UPDATE_RESET } from '../../types/userTypes'
 import '../Screens.css'
+import userEdit from '../Images/userEdit.jpg'
 
 const UserEditScreen = ({ match, history }) => {
   const userId = match.params.id
@@ -52,54 +53,69 @@ const UserEditScreen = ({ match, history }) => {
 
   return (
     <div className="userEditScreen">
-      <Link to="/admin/userslist" className="btn btn-light my-3">
-        <button className="btnback">Back to users list</button>
-      </Link>
-      <FormContainer>
-        <h1>Edit User</h1>
-        {loadingUpdate && <Loader />}
-        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
-        {loading ? (
-          <Loader />
-        ) : error ? (
-          <Message variant="danger">{error}</Message>
-        ) : (
-          <Form onSubmit={submitHandler}>
-            <Form.Group controlId="name">
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                type="name"
-                placeholder="Enter name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              ></Form.Control>
-            </Form.Group>
+      <Card className="userEditCard">
+        <Row>
+          <Col md={6}>
+            <Image className="userEditImage mt-4 ml-2" src={userEdit} />
+          </Col>
+          <Col className="UserEditFormCol" md={6}>
+            <div>
+              <h1>Edit User</h1>
+              {loadingUpdate && <Loader />}
+              {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
+              {loading ? (
+                <Loader />
+              ) : error ? (
+                <Message variant="danger">{error}</Message>
+              ) : (
+                <Form onSubmit={submitHandler}>
+                  <Form.Group controlId="name">
+                    <Form.Label className="formFieldDet">Name</Form.Label>
+                    <Form.Control
+                      className="userEditInput"
+                      type="name"
+                      placeholder="Enter name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    ></Form.Control>
+                  </Form.Group>
 
-            <Form.Group controlId="email">
-              <Form.Label>Email Address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              ></Form.Control>
-            </Form.Group>
+                  <Form.Group controlId="email">
+                    <Form.Label className="formFieldDet">
+                      Email Address
+                    </Form.Label>
+                    <Form.Control
+                      className="userEditInput"
+                      type="email"
+                      placeholder="Enter email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    ></Form.Control>
+                  </Form.Group>
 
-            <Form.Group controlId="isadmin">
-              <Form.Check
-                type="checkbox"
-                label="Is Admin"
-                checked={isAdmin}
-                onChange={(e) => setIsAdmin(e.target.checked)}
-              ></Form.Check>
-            </Form.Group>
+                  <Form.Group controlId="isadmin">
+                    <Form.Check
+                      type="checkbox"
+                      label="Is Admin?"
+                      checked={isAdmin}
+                      onChange={(e) => setIsAdmin(e.target.checked)}
+                    ></Form.Check>
+                  </Form.Group>
 
-            <Button type="submit" variant="primary">
-              Update
-            </Button>
-          </Form>
-        )}
-      </FormContainer>
+                  <Row>
+                    <Col md={7}></Col>
+                    <Col>
+                      <Button type="submit" variant="info">
+                        Update
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form>
+              )}
+            </div>
+          </Col>
+        </Row>
+      </Card>
     </div>
   )
 }
